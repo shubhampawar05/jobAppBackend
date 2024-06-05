d7db70f08dd6fbb4077547d1a60ae21d136bc40c
Shubham Chopde <shubhamchopde090@gmail.com>
2024-06-05 15:53:48 +0530

    newdb

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 const jobRoutes =require('./Routes/job')
+const dotenv = require('dotenv')
 
 app.use(express.json())
-
-mongoose.connect('mongodb+srv://Shubham05:Mongodb@1250@job.tg4lipa.mongodb.net/',{ useNewUrlParser: true, useUnifiedTopology: true })
+dotenv.config();
+mongoose.set("strictQuery",false)
+mongoose.connect(process.env.MONGOURL)
   .then(() => console.log('Connected!....'));
 
 app.use(jobRoutes)
