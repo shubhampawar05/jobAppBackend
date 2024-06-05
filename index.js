d00c0e6dc8df293cbcb9757eac8b4cd7922e2c9b
Shubham Chopde <shubhamchopde090@gmail.com>
2024-06-05 15:27:30 +0530

    error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jobRoutes =require('./Routes/job')
 
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://Shubham05:Mongodb@1250@job.tg4lipa.mongodb.net/')
+mongoose.connect('mongodb+srv://Shubham05:Mongodb@1250@job.tg4lipa.mongodb.net/',{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected!....'));
 
 app.use(jobRoutes)
